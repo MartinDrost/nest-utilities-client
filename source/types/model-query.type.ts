d13e3a2043377ebf3f2c9e2411cfc3c13cf6e00f
Martin Drost <info@martindrost.nl>
2020-09-16 14:59:33 +0200

    published 3.0.0 alpha version

--- source/types/model-query.type.ts
+++ source/types/model-query.type.ts
@@ -1,5 +1,7 @@
 import { QueryOperators } from "./query-operators.type";
 
-export type ModelQuery<ModelType = any> = Partial<
-  Record<string | keyof ModelType, Partial<Record<QueryOperators, any>>>
->;
+export type ModelQuery<ModelType = any> =
+  | {
+      [key in keyof ModelType]?: Partial<Record<QueryOperators, any>>;
+    }
+  | { [key: string]: Partial<Record<QueryOperators, any>> };
