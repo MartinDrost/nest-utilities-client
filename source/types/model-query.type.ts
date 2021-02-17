d4c8650527d44e84c8dd7f4f50ec49bca33e1fa6
Martin Drost <info@martindrost.nl>
2021-02-17 10:15:18 +0100

    improved query typing

--- source/types/model-query.type.ts
+++ source/types/model-query.type.ts
@@ -2,6 +2,14 @@ import { QueryOperators } from "./query-operators.type";
 
 export type ModelQuery<ModelType = any> =
   | {
-      [key in keyof ModelType]?: Partial<Record<QueryOperators, any>>;
+      [key in keyof ModelType]?:
+        | Record<QueryOperators, any>
+        | ModelQuery<ModelType>[]
+        | any;
     }
-  | { [key: string]: Partial<Record<QueryOperators, any>> };
+  | {
+      [key: string]:
+        | Record<QueryOperators, any>
+        | ModelQuery<ModelType>[]
+        | any;
+    };
