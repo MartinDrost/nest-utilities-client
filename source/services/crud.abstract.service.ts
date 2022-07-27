aba195a4e97e17dac0be3718250a7a9d855b34f6
Martin Drost <info@martindrost.nl>
2022-07-27 14:43:47 +0200

    refactored method notation for easier binding within f.e. react hooks

--- source/services/crud.abstract.service.ts
+++ source/services/crud.abstract.service.ts
@@ -17,24 +17,24 @@ export abstract class CrudService<ModelType, HydratedType = null> {
    * Create a new instance of the given body.
    * @param body the object which you want to create
    */
-  async post(
+  post = async (
     body: ModelType | FormData,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType, HydratedType>> {
+  ): Promise<IResponse<ModelType, HydratedType>> => {
     const response = await this.http.post(this.controller, body, options);
     response.hydrated = await this.hydrate(response.data);
 
     return response;
-  }
+  };
 
   /**
    * Create a new instances of the given bodies.
    * @param body the objects which you want to create
    */
-  async postMany(
+  postMany = async (
     body: ManyPayload<ModelType> | FormData,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType[], HydratedType[]>> {
+  ): Promise<IResponse<ModelType[], HydratedType[]>> => {
     const response = await this.http.post(
       [this.controller, "many"].join("/"),
       body,
@@ -45,17 +45,17 @@ export abstract class CrudService<ModelType, HydratedType = null> {
     );
 
     return response;
-  }
+  };
 
   /**
    * Get a model by its id.
    * @param id the id of the model to get
    * @param options
    */
-  async get(
+  get = async (
     id: string,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType, HydratedType>> {
+  ): Promise<IResponse<ModelType, HydratedType>> => {
     const response = await this.http.get(
       [this.controller, id].join("/"),
       options
@@ -63,17 +63,17 @@ export abstract class CrudService<ModelType, HydratedType = null> {
     response.hydrated = await this.hydrate(response.data);
 
     return response;
-  }
+  };
 
   /**
    * Get multiple models by their id.
    * @param ids the ids of the models to get
    * @param options
    */
-  async getMany(
+  getMany = async (
     ids: string[],
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType[], HydratedType[]>> {
+  ): Promise<IResponse<ModelType[], HydratedType[]>> => {
     const response = await this.http.get<any[]>(this.controller, {
       ...options,
       match: { ...options.match, _id: { $in: ids } },
@@ -95,47 +95,47 @@ export abstract class CrudService<ModelType, HydratedType = null> {
     );
 
     return response;
-  }
+  };
 
   /**
    * Get all models.
    * @param options
    */
-  async getAll(
+  getAll = async (
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType[], HydratedType[]>> {
+  ): Promise<IResponse<ModelType[], HydratedType[]>> => {
     const response = await this.http.get(this.controller, options);
     response.hydrated = await Promise.all(
       response.data.map((model) => this.hydrate(model))
     );
 
     return response;
-  }
+  };
 
   /**
    * Overwrite the instance of the given model with the provided body.
    * The model to overwrite is defined with by the (_)id in the body object.
    * @param body the object which you want to overwrite
    */
-  async put(
+  put = async (
     body: ModelType | FormData,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType, HydratedType>> {
+  ): Promise<IResponse<ModelType, HydratedType>> => {
     const response = await this.http.put(this.controller, body, options);
     response.hydrated = await this.hydrate(response.data);
 
     return response;
-  }
+  };
 
   /**
    * Overwrite the instances of the given models with the provided bodies.
    * The models to overwrite are defined with by the (_)id in the body objects.
    * @param body the objects which you want to overwrite
    */
-  async putMany(
+  putMany = async (
     body: ManyPayload<ModelType> | FormData,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType[], HydratedType[]>> {
+  ): Promise<IResponse<ModelType[], HydratedType[]>> => {
     const response = await this.http.put(
       [this.controller, "many"].join("/"),
       body,
@@ -146,32 +146,32 @@ export abstract class CrudService<ModelType, HydratedType = null> {
     );
 
     return response;
-  }
+  };
 
   /**
    * Merge the provided body with the instance of the given model in the DB.
    * The model to overwrite is defined with by the (_)id in the body object.
    * @param body the object which you want to use to merge with the model
    */
-  async patch(
+  patch = async (
     body: Partial<ModelType> | FormData,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType, HydratedType>> {
+  ): Promise<IResponse<ModelType, HydratedType>> => {
     const response = await this.http.patch(this.controller, body, options);
     response.hydrated = await this.hydrate(response.data);
 
     return response;
-  }
+  };
 
   /**
    * Merge the provided bodies with the instances of the given models in the DB.
    * The models to overwrite are defined with by the (_)id in the body objects.
    * @param body the objects which you want to use to merge with the models
    */
-  async patchMany(
+  patchMany = async (
     body: ManyPayload<Partial<ModelType>> | FormData,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType[], HydratedType[]>> {
+  ): Promise<IResponse<ModelType[], HydratedType[]>> => {
     const response = await this.http.patch(
       [this.controller, "many"].join("/"),
       body,
@@ -182,16 +182,16 @@ export abstract class CrudService<ModelType, HydratedType = null> {
     );
 
     return response;
-  }
+  };
 
   /**
    * Delete a model by its id.
    * @param id the id of the model to delete
    */
-  async delete(
+  delete = async (
     id: string,
     options: IHttpOptions<ModelType> = {}
-  ): Promise<IResponse<ModelType, HydratedType>> {
+  ): Promise<IResponse<ModelType, HydratedType>> => {
     const response = await this.http.delete(
       [this.controller, id].join("/"),
       options
@@ -199,7 +199,7 @@ export abstract class CrudService<ModelType, HydratedType = null> {
     response.hydrated = await this.hydrate(response.data);
 
     return response;
-  }
+  };
 
   /**
    * Overridable method used to hydrate response objects. This method is
@@ -210,7 +210,7 @@ export abstract class CrudService<ModelType, HydratedType = null> {
    * @param model
    * @returns
    */
-  async hydrate(model: ModelType): Promise<HydratedType> {
+  hydrate = async (model: ModelType): Promise<HydratedType> => {
     return null as any;
-  }
+  };
 }
